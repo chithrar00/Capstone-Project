49135f6efc4d8a7ae0b96a0ede0719bfd05aa2ef
Chithra Raji <chithrar00@gmail.com>
2024-08-12 21:45:11 +0530

    Made changes to add an application and print hello

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# View function to print Hello.
+def sayHello(request):
+    return HttpResponse('Hello World!')
